19194f34cf043a5e09b902b9b17633340f0eabb9
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-08-02 17:52:08 +0100

    fix(connectionString)

--- server/db.js
+++ server/db.js
@@ -1,12 +1,20 @@
 import knex from 'knex';
 
+// for local DB
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'postgres',
+//     password: 'postgres',
+//     database: 'facebrain'
+//   }
+// });
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'facebrain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
